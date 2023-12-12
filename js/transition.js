05ab5d23553ba185d10b9418b99b768c291d581c
Jacob Blair <jacob@Jacobs-MacBook-Pro.local>
2023-12-12 13:44:02 -0700

    fix page routing on pages

--- js/transition.js
+++ js/transition.js
@@ -8,8 +8,12 @@ This is my own work
 const fadeOut = (page) => {
   $("body").attr("style", "opacity: 0 !important;"); // set the body's opacity to 0 including transition
   setTimeout(() => {
+    if (this.window.origin != "http://127.0.0.1:5501") {
+      window.location.href = this.window.origin + "/cst120-milestone7/" + page;
+    } else {
+      window.location.href = this.window.origin + "/" + page;
+    }
     // once the transition is done, go to the provided page (only works for on-site pages!)
-    window.location.href = this.window.origin + "/" + page;
   }, 350); // 350 ms
 };
 // attr is used because of bootstrap
